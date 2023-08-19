04e296fb21aa08ca185f2248457281595c56c41a
nck.974 <nck.974@gmail.com>
2023-08-19 10:13:09 +0200

    [FEATURE] Allow CORS of delete methods

--- src/main/java/dev/nichoko/diogenes/config/CorsConfig.java
+++ src/main/java/dev/nichoko/diogenes/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedMethods("GET", "HEAD", "OPTIONS", "DELETE", "POST", "PUT")
                 .allowedOrigins("http://localhost:4200")
                 .allowCredentials(true);
     }
