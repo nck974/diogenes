61bddc157a3b2b46680305dfffab34df9b6b2a69
nck.974 <nck.974@gmail.com>
2024-01-18 22:41:27 +0100

    [FEATURE] Cache images to dont overload the server

--- diogenes/src/main/java/dev/nichoko/diogenes/controller/FilesController.java
+++ diogenes/src/main/java/dev/nichoko/diogenes/controller/FilesController.java
@@ -1,7 +1,10 @@
 package dev.nichoko.diogenes.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +28,13 @@ public class FilesController {
     @GetMapping("/images/{imageName}")
     public ResponseEntity<Resource> serveImage(@PathVariable String imageName) {
         Resource resource = fileStorageService.readItemImage(imageName);
+        
+        // Set caching headers
+        CacheControl cacheControl = CacheControl.maxAge(30, TimeUnit.DAYS);
+        
         return ResponseEntity.ok()
                 .contentType(FileTypeChecker.guessImageContentType(imageName))
+                .cacheControl(cacheControl)
                 .body(resource);
 
     }
